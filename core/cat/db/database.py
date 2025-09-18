7b6f88d30e60b3e1802d20dfd6f60490a495dcc8
Giovanni Albero <giovanni@smartstrategy.eu>
2025-09-18 18:32:44 +0200

    Added async redis connection

--- core/cat/db/database.py
+++ core/cat/db/database.py
@@ -1,4 +1,5 @@
 import redis
+import redis.asyncio as aioredis
 
 from cat.utils import singleton
 from cat.env import get_env
@@ -11,6 +12,31 @@ DEFAULT_SYSTEM_KEY = "system"
 class Database:
     def __init__(self):
         self.db = self.get_redis_client()
+        self.aiodb = self.get_aioredis_client()
+
+    def get_aioredis_client(self) -> aioredis.Redis:
+        password = get_env("CCAT_REDIS_PASSWORD")
+        tls = get_env("CCAT_REDIS_TLS")
+
+        if password:
+            return aioredis.Redis(
+                host=get_env("CCAT_REDIS_HOST"),
+                port=int(get_env("CCAT_REDIS_PORT")),
+                db=get_env("CCAT_REDIS_DB"),
+                password=password,
+                encoding="utf-8",
+                decode_responses=True,
+                ssl=tls
+            )
+
+        return aioredis.Redis(
+            host=get_env("CCAT_REDIS_HOST"),
+            port=int(get_env("CCAT_REDIS_PORT")),
+            db=get_env("CCAT_REDIS_DB"),
+            encoding="utf-8",
+            decode_responses=True,
+            ssl=tls
+        )
 
     def get_redis_client(self) -> redis.Redis:
         password = get_env("CCAT_REDIS_PASSWORD")
@@ -39,3 +65,6 @@ class Database:
 
 def get_db() -> redis.Redis:
     return Database().db
+
+def get_aiodb() -> aioredis.Redis:
+    return Database().aiodb
